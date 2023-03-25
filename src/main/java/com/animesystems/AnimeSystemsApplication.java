a7917b94c2a3fc187b24daf29fc429dc6c7602e9
Michael Chatzidakis <xatzidak.m@gmail.com>
2023-03-25 18:15:46 +0200

    Initial commit

---
+++ src/main/java/com/animesystems/AnimeSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.animesystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animesystems/AnimeSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
