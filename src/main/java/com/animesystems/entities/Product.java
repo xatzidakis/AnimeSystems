75673d8e8af6ffc3de8679bcfc1572fbc9004095
Michael Chatzidakis <xatzidak.m@gmail.com>
2023-03-26 18:30:47 +0300

    feat: Created Product entity

---
+++ src/main/java/com/animesystems/entities/Product.java
@@ -0,0 +1,94 @@
+package com.animesystems.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private BigDecimal price;
+    private String description;
+    private String title;
+    private String photo;
+    private String category;
+    private String color;
+    private String size;
+    private Integer available_quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getAvailable_quantity() {
+        return available_quantity;
+    }
+
+    public void setAvailable_quantity(Integer available_quantity) {
+        this.available_quantity = available_quantity;
+    }
+}
