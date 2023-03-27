c8434dacd5f1a26ba47f1961465289979ef3d520
Michael Chatzidakis <xatzidak.m@gmail.com>
2023-03-27 07:28:55 +0300

    feat: Added search methods on service and repository

--- src/main/java/com/animesystems/services/ProductServiceImpl.java
+++ src/main/java/com/animesystems/services/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -38,5 +39,40 @@ public class ProductServiceImpl implements ProductService {
     public Page<Product> getAllProducts(int page, int size) {
         PageRequest paging = PageRequest.of(page, size);
         return productRepository.findAll(paging);
+    }
+
+    public Product getProductById(Integer id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElse(null);
+    }
+
+    public Page<Product> getContainsTitle(String title, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findByTitleContainingIgnoreCase(title, paging);
+    }
+
+    public Page<Product> getContainsDescription(String description, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findByDescriptionContainingIgnoreCase(description, paging);
+    }
+
+    public Page<Product> getBetweenPrice(BigDecimal minPrice, BigDecimal maxPrice, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findByPriceBetween(minPrice, maxPrice, paging);
+    }
+
+    public Page<Product> getByCategory(String category, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findByCategoryEquals(category, paging);
+    }
+
+    public Page<Product> getByColor(String color, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findByColorEquals(color, paging);
+    }
+
+    public Page<Product> getBySize(String productSize, int page, int size) {
+        PageRequest paging = PageRequest.of(page, size);
+        return productRepository.findBySizeEquals(productSize, paging);
     }
 }
