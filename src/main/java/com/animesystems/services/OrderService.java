400ed9b1156abe2219e7869330aeedce79911302
Giorgos Iliopoulos <iligeoili@csd.auth.gr>
2023-03-28 21:07:59 +0300

    For review

--- src/main/java/com/animesystems/services/OrderService.java
+++ src/main/java/com/animesystems/services/OrderService.java
@@ -3,9 +3,9 @@ package com.animesystems.services;
 import com.animesystems.entities.Order;
 import com.animesystems.entities.ProductOrder;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public interface OrderService {
     public void deleteOrder(Integer id);
     public Order createOrUpdateOrder(Integer id, Order order) throws Exception;
