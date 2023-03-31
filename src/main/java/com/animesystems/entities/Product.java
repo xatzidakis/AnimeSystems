ec4913ea451dd145cc0283d22c49ece6a4e2e74a
dimvourd <108293215+dimvourd@users.noreply.github.com>
2023-03-31 19:21:34 +0300

    product details

--- src/main/java/com/animesystems/entities/Product.java
+++ src/main/java/com/animesystems/entities/Product.java
@@ -31,97 +31,23 @@ public class Product {
     public enum Category {
         Figures, Clothing, Books
     }
+
     @Enumerated(EnumType.STRING)
     private Category category;
-
 
     public enum Color{
         Black, White, Red, Yellow, Blue, Green
     }
+
     @Enumerated(EnumType.STRING)
     private Color color;
 
     public enum Size{ XS, S, M, L, XL}
+
     @Enumerated(EnumType.STRING)
     private Size size;
     private Integer available_quantity;
 
-
-
-
-//    public Product(){}
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public BigDecimal getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(BigDecimal price) {
-//        this.price = price;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getPhoto() {
-//        return photo;
-//    }
-//
-//    public void setPhoto(String photo) {
-//        this.photo = photo;
-//    }
-//
-//    public String getCategory() {
-//        return category;
-//    }
-//
-//    public void setCategory(String category) {
-//        this.category = category;
-//    }
-//
-//    public String getColor() {
-//        return color;
-//    }
-//
-//    public void setColor(String color) {
-//        this.color = color;
-//    }
-//
-//    public String getSize() {
-//        return size;
-//    }
-//
-//    public void setSize(String size) {
-//        this.size = size;
-//    }
-//
-//    public Integer getAvailable_quantity() {
-//        return available_quantity;
-//    }
-//
-//    public void setAvailable_quantity(Integer available_quantity) {
-//        this.available_quantity = available_quantity;
-//    }
 }
 
 
